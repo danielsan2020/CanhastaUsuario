2dab1b27989af552874dd48ac1a2f6d686a2f19e
danielsan2020 <jdanielsancheztaza@gmail.com>
2020-07-16 21:51:32 -0500

    actualizado

--- js/script.js
+++ js/script.js
@@ -125,5 +125,21 @@ $('#simple-timer').syotimer({
         }
       }); /*ready*/
 
+      $('#Modalfactura').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget) // Button that triggered the modal
+        var recipient = button.data('whatever') // Extract info from data-* attributes
+        // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+        // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+        var modal = $(this)
+        modal.find('.modal-title').text('New message to ' + recipient)
+        modal.find('.modal-body input').val(recipient)
+      })
+
+      // CALCULADORA ENVIO
+      // <!-- Ejemplo: 8 km | 8km-3km= 5km * $8=$40 + $27.43 = * 0.16 -->
+      let enviocalculo = "";
+      let resultadoenvio = (enviocalculo - 3) (* 8) (+ 27.43) (* 0.16);
+      let final = resultadoenvio
+
 
 })(jQuery);
